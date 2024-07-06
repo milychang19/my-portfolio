0306726d9cfada20b486e8e43b07f58d2160cf51
Emily Chang <emilychang@Emilys-Air-7.Home>
2024-07-06 11:13:28 -0400

    title svg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,10 @@ import portrait from '../elements/portrait.jpg';
 const Home = () => {
   return (
     <section id="home">
-      <h1>Emily Chang</h1>
+      <svg viewBox="0 0 500 100">
+        <path id="curve" fill="transparent" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+        <text x="20%"><textPath href="#curve">Emily Chang</textPath></text>
+      </svg>
       <img src={portrait} alt="" id="portrait"/>
       <p>Resume</p>
     </section>
