21f95e8da1456f56a1279fdee79909b1dca9c4be
Emily Chang <emilychang@Emilys-Air-7.Home>
2024-07-08 15:33:46 -0400

    edit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <section id="about">
       <h1 class="about-me" id="move-left">About</h1>
       <h1 class="about-me" id="move-right">Me</h1>
-      <p>“The only way to do great work is to love what you do”-Steve Jobs
+      <p class="me">“The only way to do great work is to love what you do”-Steve Jobs
         I like to bring joy and creativity into my work. As a computer science students, I love challenges and creating good design to solve real-world problems. I like to get inspired by good arts and appreciate good designs.
         Outside of school, I’m probably enjoying a good movie or out at a gallery.</p>
     </section>
