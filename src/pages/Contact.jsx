1fa07445dd3f191087c1d2888ee3f3362c31a77d
Emily Chang <emilychang@Emilys-Air-7.Home>
2024-07-08 13:54:11 -0400

    project card

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import logo from "../elements/contact-logo.png";
 
 const Contact = () => {
   return (
     <section id="contact">
-      <h1>Contact Me</h1>
+      <h1 id="contact-me">Contact Me</h1>
       <p>github</p>
+      <button>Send</button>
+      <img src={logo} alt="" title="contact-logo" id="face"></img>
     </section>
   );
 };
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,22 @@ const Projects = () => {
   return (
     <section id="projects">
       <h1>project highlights</h1>
-      <p>...</p>
+      <div class="project-container">
+        <div class="project-card">
+          <h3>ResShiftGenerator</h3>
+          
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a...</p>
+          </div>
+        <div class="project-card">
+          <h3>BattleshipGameAI</h3>
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a...</p>
+          </div>
+        <div class="project-card">
+          <h3>MancalaGame</h3>
+          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a...</p>
+          </div>
+      </div>
+      
     </section>
   );
 };
