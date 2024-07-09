3253a39da84c9d65bdb880ecdf4a7cdbe8e5bbfd
Emily Chang <emilychang@Emilys-Air-7.Home>
2024-07-09 18:29:44 -0400

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,9 +10,9 @@ const Contact = () => {
       <p>github</p>
       <button>Send</button>
       <img src={logo} alt="" title="contact-logo" id="face"></img>
-      <a href="https://www.linkedin.com/in/emily-chang-485962234/" target="_blank" rel="noreferrer" title="EmilyChang-LinkedIn" className="icon"><AiFillLinkedin /></a>
-      <a href="https://github.com/milychang19?ocid=AIDcmmli8vlwie_SEM__k_CjwKCAjw-O6zBhASEiwAOHeGxWZJoswgM1wM05bjiyrXGOtRXjaDnea15Is_IDrOCGaAYogtxg1hFBoCh1sQAvD_BwE_k_" target="_blank" rel="noreferrer" title="EmilyChang-GitHub" className="icon"><AiFillGithub /></a>
-      <a href="#contact" title="EmilyChang-Email" className="icon"><AiFillMail /></a>
+      <a href="https://www.linkedin.com/in/emily-chang-485962234/" target="_blank" rel="noreferrer" title="Emily Chang" className="icon"><AiFillLinkedin /></a>
+      <a href="https://github.com/milychang19?ocid=AIDcmmli8vlwie_SEM__k_CjwKCAjw-O6zBhASEiwAOHeGxWZJoswgM1wM05bjiyrXGOtRXjaDnea15Is_IDrOCGaAYogtxg1hFBoCh1sQAvD_BwE_k_" target="_blank" rel="noreferrer" title="Emily Chang" className="icon"><AiFillGithub /></a>
+      <a href="mailto:emily0109.ec@gmail.com" title="emily0109.ec@gmail.com" className="icon"><AiFillMail /></a>
 
     </section>
   );
