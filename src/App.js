5708b2a17ae4a596acdbf2e598982abdd1c33c1e
P541M <videna.psalmeleazar@gmail.com>
2024-08-04 18:40:09 -0400

    refactor: formatting of files, adjusted honepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
-import Header from './pages/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Journey from './pages/Journey';
-import Contact from './pages/Contact';
-import Footer from './pages/Footer';
+import Header from "./pages/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Journey from "./pages/Journey";
+import Contact from "./pages/Contact";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
     <div className="font-text bg-beige text-center text-calc-size">
-      <Header/>
+      <Header />
       <Home />
       <About />
       <Projects />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,60 +4,59 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'green': '#095D40', //name title
-        'beige': '#FFFFEC', //background
-        'blue': '#034AB4', //about me
-        'yellow': '#FFA857', //icon
-        'brown': '#1F1A12', //text
-        'matcha': '#EFEEDD', //header & contact card
-        'sage': '#F3F7E4', //project cards
-        'grey': '#95A29D',
-        'sun': '#FDB777', //hovered icon
-        'forest': '#074D35', //hovered button
-        'coral': '#CD807B', //tag1
-        'mint': '#7DA68D', //tag2
-        'star': '#F4CD94', //tag3
-        'sky': '#AEBFD5', //project name
+        green: "#095D40", //name title
+        beige: "#FFFFEC", //background
+        blue: "#034AB4", //about me
+        yellow: "#FFA857", //icon
+        brown: "#1F1A12", //text
+        matcha: "#EFEEDD", //header & contact card
+        sage: "#F3F7E4", //project cards
+        grey: "#95A29D",
+        sun: "#FDB777", //hovered icon
+        forest: "#074D35", //hovered button
+        coral: "#CD807B", //tag1
+        mint: "#7DA68D", //tag2
+        star: "#F4CD94", //tag3
+        sky: "#AEBFD5", //project name
       },
       boxShadow: {
-        'card': '0px 0px 10px rgba(0, 0, 0, 0.1)',
+        card: "0px 0px 10px rgba(0, 0, 0, 0.1)",
       },
       dropShadow: {
-        'card': '0px 20px 10px rgba(0, 0, 0, 0.03)',
+        card: "0px 20px 10px rgba(0, 0, 0, 0.03)",
       },
       fontSize: {
-        'calc-size': 'calc(10px + 2vmin)',
+        "calc-size": "calc(10px + 2vmin)",
       },
       keyframes: {
-        'fade-in-zoom': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(20px)',
+        "fade-in-zoom": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)",
           },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
           },
         },
       },
       animation: {
-        'fade-in-zoom1': 'fade-in-zoom 0.5s ease-out',
-        'fade-in-zoom2': 'fade-in-zoom 0.6s ease-out',
-        'fade-in-zoom3': 'fade-in-zoom 0.7s ease-out',
-        'fade-in-zoom4': 'fade-in-zoom 0.8s ease-out',
+        "fade-in-zoom1": "fade-in-zoom 0.5s ease-out",
+        "fade-in-zoom2": "fade-in-zoom 0.6s ease-out",
+        "fade-in-zoom3": "fade-in-zoom 0.7s ease-out",
+        "fade-in-zoom4": "fade-in-zoom 0.8s ease-out",
       },
     },
     fontFamily: {
-      title: ['Yeseva One', 'serif'],
-      sub: ['Fraunces', 'serif'],
-      text: ['Josefin Sans', 'sans-serif'],
+      title: ["Yeseva One", "serif"],
+      sub: ["Fraunces", "serif"],
+      text: ["Josefin Sans", "sans-serif"],
     },
     fontWeight: {
-      normal: '400',
-      bold: '700',
-      xbold: '800',
-    }
+      normal: "400",
+      bold: "700",
+      xbold: "800",
+    },
   },
   plugins: [],
-}
-
+};
