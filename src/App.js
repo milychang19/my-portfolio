f9ebdea9d4653edae2250f4abb2b19aa4363a34a
Emily Chang <emily0109.ec@gmail.com>
2025-01-10 01:17:54 -0500

    fix hash router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./pages/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
