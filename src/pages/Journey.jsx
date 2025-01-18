c756bd7f6c53113b301ad80e5f12597656098fc4
Emily Chang <emily0109.ec@gmail.com>
2025-01-18 14:59:54 -0500

    new resume

--- src/pages/Journey.jsx
+++ src/pages/Journey.jsx
@@ -73,7 +73,7 @@ const Journey = () => {
             
             <p className={`font-sub text-base font-[600] inline-block italic duration-200 ${hoveredIndex !== null && hoveredIndex === index ? 'text-green duration-200':''}`}>{experience.company}</p>
             {experience.company === "@Magnet Forensics" && (
-              <MdArrowOutward className={` inline-block size-7 ${hoveredIndex !== null && hoveredIndex === index ? 'fill-green duration-200':''}`}/>
+              <MdArrowOutward className={`inline-block size-7 ${hoveredIndex !== null && hoveredIndex === index ? 'fill-green duration-200':''}`}/>
               // <button className="bg-star text-brown bg-opacity-70 text-sm px-10 py-2 rounded-full shadow-sm hover:bg-opacity-100 transition transition-500 animate-fade-in-zoom3" onClick={() =>navigate("/F24-work-term-report")}>View Work Term Report</button>
               // <button className="bg-transparent border-brown border-2 border-opacity-10 text-brown text-sm px-10 py-2 rounded-full shadow-sm hover:bg-brown hover:bg-opacity-20 hover:text-beige transition transition-500 animate-fade-in-zoom3" onClick={handleClick}>View Work Term Report</button>
             )}
